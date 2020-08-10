eb0062abb637b9245e7e4367687447c67d3c2db7
Rafael Pianaro <rafaelpianaro@gmail.com>
2020-08-10 15:28:16 -0300

    simple return for server.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,1 +1,9 @@
- console.log('Hello World')
+ import express from 'express'
+
+ const app = express()
+
+ app.get('/users', (request, response) => {
+     return response.send('Hello World')
+ })
+
+ app.listen(3333)
