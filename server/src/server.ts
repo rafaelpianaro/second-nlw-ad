46fdf3cc2b0c595b040b156a68508c8f08b70b31
Rafael Pianaro <rafaelpianaro@gmail.com>
2020-08-12 11:15:44 -0300

    clean server.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,14 +4,8 @@
 
  app.use(express.json())
 
- app.get('/users', (request, response) => {
-     console.log(request.body)
-
-    const users = [
-        {name: 'Doritos', age: 3},
-        {name: 'Duna', age: 6}
-    ] 
-    return response.json(users)
+ app.get('/users', (request, response) => {     
+    return response.json({ message: 'Hello World'})
  })
 
  app.listen(3333)
